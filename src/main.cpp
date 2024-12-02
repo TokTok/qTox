d8a36417a029a2f6fff53157d8ccc0bb8925f34e
iphydf <iphydf@users.noreply.github.com>
2024-12-02 22:51:58 +0000

    test: Wait for debugger to attach in main.
    This is just to support an attempt at debugging qTox on Android 7.

--- src/main.cpp
+++ src/main.cpp
@@ -8,8 +8,20 @@
 #include <QDebug>
 #include <QGuiApplication>
 
+#ifdef Q_OS_ANDROID
+#include <unistd.h>
+
+static volatile bool startTheProgram = false;
+#endif
+
 int main(int argc, char* argv[])
 {
+#ifdef Q_OS_ANDROID
+    while (!startTheProgram) {
+        // Wait for the debugger to attach
+        sleep(1);
+    }
+#endif
     AppManager appManager(argc, argv);
     int errorcode = appManager.run();
 
